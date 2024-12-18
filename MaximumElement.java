8f0ea2767c6bd5c4a367804d1c63307b321e1d07
Krish Sharma <krishsharma9005@gmail.com>
2024-12-18 13:11:05 +0530

    MaximumElement

---
+++ MaximumElement.java
@@ -0,0 +1,26 @@
+package Arrays2D;
+import java.util.Scanner;
+
+public class MaximumElement {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+
+        //values
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<arr.length; i++) {
+            for(int j = 0; j<arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++) {
+            for(int j = 0; j<arr[0].length; j++) {
+                if(arr[i][j]>max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum Element: " +max);
+    }
+}
