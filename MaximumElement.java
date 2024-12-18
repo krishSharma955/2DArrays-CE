85e2facf9d2a7775a9924d2bc66d01b58d49cabb
Krish Sharma <krishsharma9005@gmail.com>
2024-12-18 16:17:34 +0530

    Maximum & Minimum Element

--- MaximumElement.java
+++ MaximumElement.java
@@ -14,14 +14,19 @@ public class MaximumElement {
         }
 
         int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int i = 0; i<arr.length; i++) {
             for(int j = 0; j<arr[0].length; j++) {
                 if(arr[i][j]>max) {
                     max = arr[i][j];
                 }
+                if(arr[i][j]<min) {
+                    min = arr[i][j];
+                }
             }
         }
         System.out.println("Maximum Element: " +max);
+        System.out.println("Minimum Element: " +min);
         sc.close();
     }
 }
