9963642623a2f8953e11974200ab2414a6bef0ea
Krish Sharma <krishsharma9005@gmail.com>
2024-12-22 18:18:49 +0530

    Largest in 2D

--- Largest.java
+++ Largest.java
@@ -23,7 +23,7 @@ public class Largest {
                 matrix[i][j] = sc.nextInt();
             }
         }
-        //output
+        //matrix
         for(int i = 0; i<matrix.length; i++) {
             for(int j = 0; j<matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
